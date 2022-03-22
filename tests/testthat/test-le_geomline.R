e985da69c2184440956b489f4eacf8fd0dbb4c55
Miguel Ayala <miguel.ayala.au@gmail.com>
2022-03-22 16:46:20 +1100

    fix date

--- tests/testthat/test-le_geomline.R
+++ tests/testthat/test-le_geomline.R
@@ -1,7 +1,7 @@
 test_that("le_geomline works", {
   l <- economics_long[1:3]
   l_a <- le_geomline(l, ttl = "US macro variables",
-                     "%", c("01/01/2004", "01/12/2016"), c(0, 9, 2),
+                     "%", c("2004/1/1", "2016/12/1"), c(0, 9, 2),
                      leg = c("x", "y"),
                      leg_pos = c(0.25, 0.88), thm = 'le_theme')
 })
