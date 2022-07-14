62ca3963df7cbd8d792cd4caa18c545f88755b9d
Miguel Ayala <miguel.ayala.au@gmail.com>
2022-07-14 17:14:07 +1000

    made changes

--- R/le_theme.R
+++ R/le_theme.R
@@ -28,9 +28,9 @@ le_theme <- function(leg_pos = c(0.9,0.9), lescale = 0.8, flip = 0){
   #  font <- "Roboto Condensed"
   #}
 
-  if(flip==1){pm <- margin(0,30,0,5)*lescale}else{pm <- margin(0,10,0,5)*lescale}
+  #if(flip==1){pm <- margin(0,30,0,5)*lescale}else{pm <- margin(0,10,0,5)*lescale}
 
-  theme_foundation(base_size = 14, base_family = "sans") +#%+replace%
+  theme_foundation(base_size = 14, base_family = "sans") + #%+replace%
 
     theme(text = element_text(#angle=0,
                               #size = 20*lescale,
@@ -39,21 +39,23 @@ le_theme <- function(leg_pos = c(0.9,0.9), lescale = 0.8, flip = 0){
                               #family = font
                               ),
           line = element_line(colour = "black", size = 1*lescale),
+          rec = element_rect(fill = "#F0F0F0",
+                             linetype = 0, colour = NA),
 
           axis.title.y.left = element_text(angle = 0,margin=unit(c(0,-1.3,0,0.75)*lescale, "cm")),
           axis.title.y.right = element_text(angle=0,vjust=1.08,hjust=0,margin=unit(c(0,0,0,-1.5)*lescale, "cm"), size=18*lescale),
           axis.title.x = element_blank(),
 
-          axis.ticks = element_line(size=1*lescale, colour= "#495057"),
-          axis.ticks.length = unit(0.15*lescale, "cm"),
+          axis.ticks = element_blank(),#element_line(size=1*lescale, colour= "#D2D2D2"),
+          #axis.ticks.length = unit(0.15*lescale, "cm"),
 
           axis.text = element_text(angle = 0,colour="#495057",size=18*lescale),
           axis.text.x = element_text(margin=unit(c(0.35,0.35,0,0.5)*lescale, "cm")),
           axis.text.y = element_text(margin=unit(c(0.5,0.35,0.5,0.5)*lescale, "cm")),
           axis.text.y.right= element_text(margin=unit(c(0.5,0.5,0.5,0.35)*lescale, "cm")),
 
-          axis.line.x = element_line(size=1*lescale, colour= "#495057"),
-          axis.line.y = element_line(size=1*lescale, colour= "#495057"),
+          axis.line = element_blank(),#element_line(size=1*lescale, colour= "#495057"),
+          #axis.line.y = element_blank(),#element_line(size=1*lescale, colour= "#495057"),
 
           legend.key = element_rect(colour=NA, fill=NA),
           legend.margin = margin(0,0,0,0),
@@ -62,16 +64,21 @@ le_theme <- function(leg_pos = c(0.9,0.9), lescale = 0.8, flip = 0){
           legend.background = element_rect(),
           legend.spacing.x = unit(0,'cm'),
           legend.spacing.y = unit(0,'cm'),
-          legend.position = leg_pos,
+          #legend.position = leg_pos,
 
           plot.title = element_text(margin=unit(c(0.2,0,0.15,0)*lescale,"cm"),size=24*lescale, hjust=0.0, face = 'bold'),
           plot.subtitle = element_text(hjust=0.0,margin=unit(c(0.15,0,0.5,0)*lescale,"cm"),size=18*lescale),
           plot.caption = element_text(hjust=0.0,size=14*lescale,margin=unit(c(0.25,0,0.15,0)*lescale,"cm")),
-          plot.margin = pm,
+          #plot.margin = pm,
+          legend.position = "bottom",
+          legend.direction = "horizontal",
+          legend.box = "vertical",
 
-          panel.background = element_rect(fill = "#F0F0F0"), #light gray
+          #panel.background = element_rect(fill = "#F0F0F0"), #light gray
+          panel.grid = element_line(colour = NULL),
           panel.grid.major = element_line(colour = "#D2D2D2"),
           panel.grid.minor = element_blank(),
+          plot.margin = unit(c(1, 1, 1, 1), "lines"),
 
           strip.background = element_rect()
     )
