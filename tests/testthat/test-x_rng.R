b7547ad78c44d563b9d5fb13cb64080a560de4d5
miguel ayala <miguel.ayala.au@gmail.com>
2025-06-08 22:17:19 +1100

    remove tests

--- tests/testthat/test-x_rng.R
+++
@@ -1,9 +0,0 @@
-test_that("x_range() converts numeric into date class", {
-  expect_equal(x_rng(c(2004, 2016), ggplot2::economics_long), c("2004-01-01","2015-04-01"))
-
-})
-
-test_that("x_range() errors if input lenght > 2", {
-  expect_error(x_rng(c(2004, 2016), ggplot2::economics_long), ",")
-
-})
