25b7b542daf0919db4175c354d357e607323876f
Miguel Ayala <miguel.ayala.au@gmail.com>
2025-10-19 18:24:12 +1100

    add plot label fun and replace unit with margin in theme

--- R/le_theme.R
+++ R/le_theme.R
@@ -3,7 +3,6 @@
 #'Sets chart borders, font, title size and positioning for html publications
 #'
 #' @param scale Size of legend text and title.
-#' @param flip Binary to flip margins
 #' @param rm_x_leg logical value indicating whether x label be removed from chart
 #' @param rm_y_leg logical value indicating whether y label be removed from chart
 #'
@@ -17,67 +16,120 @@
 #'@import ggplot2
 #'@import ggthemes
 
-le_theme <- function(scale = 0.8, flip = 0,
-                     rm_x_leg = FALSE, rm_y_leg = FALSE){
-
-  if(flip==1){pm <- margin(0,30,0,5)*scale}else{pm <- margin(0,10,0,5)*scale}
+le_theme <- function(scale = 0.8,
+                     rm_x_leg = FALSE,
+                     rm_y_leg = FALSE){
 
   if (rm_x_leg == TRUE) {
     x_leg <- element_blank()
-  }else{x_leg <- element_text(margin=unit(c(0,-0.1,0,0.75)*scale, "cm"))
+
+  }else{x_leg <- element_text(
+    margin = margin(t=0.22*scale, r=0, b=-0.1*scale, l=0.15*scale, unit = "cm")
+    )
   }
 
   if (rm_y_leg == TRUE) {
     y_leg <- element_blank()
-  }else{y_leg <- element_text(angle = 90,margin=unit(c(0,-0.1,0,0.75)*scale, "cm"))
+
+  }else{y_leg <- element_text(
+    angle = 90,
+    margin = margin(t=0, r=0, b=-0.1*scale, l=0.05*scale, unit = "cm")
+    )
   }
 
 
   theme_foundation(base_size = 14, base_family = "sans") +
 
-    theme(text = element_text(
-                              colour = '#3C3C3C',#Dark Gray
-                              ),
-          line = element_line(colour = "black", linewidth = 1*scale),
-          rect = element_rect(fill = "#F0F0F0",
-                             linetype = 0, colour = NA),
+    theme(
+      text = element_text(colour = '#3C3C3C'),
+      line = element_line(colour = "black", linewidth = 1*scale),
+      rect = element_rect(fill = "#F0F0F0", linetype = 0, colour = NA),
 
-          axis.title.y.left = y_leg,
-          axis.title.y.right = element_text(angle=0,vjust=1.08,hjust=0,margin=unit(c(0,0,0,-1.5)*scale, "cm"), size=18*scale),
-          axis.title.x = x_leg,
+      axis.title.y.left = y_leg,
 
-          axis.ticks = element_blank(),
+      axis.title.y.right = element_text(
 
-          axis.text = element_text(angle = 0,colour="#495057",size=18*scale),
-          axis.text.x = element_text(margin=unit(c(0.35,0.35,0,0.5)*scale, "cm")),
-          axis.text.y = element_text(margin=unit(c(0.5,0.35,0.5,0.5)*scale, "cm")),
-          axis.text.y.right= element_text(margin=unit(c(0.5,0.5,0.5,0.35)*scale, "cm")),
+        angle = 0,
+        vjust = 1.08,
+        hjust = 0,
+        margin = margin(t=0, r=0, b=0, l=-1.5*scale, unit = "cm"),
+        size = 18*scale
+        ),
 
-          axis.line = element_blank(),
+      axis.title.x = x_leg,
 
-          legend.key = element_rect(colour=NA, fill=NA),
-          legend.margin = margin(0,0,0,0),
-          legend.text = element_text(size = 18*scale),
-          legend.title = element_blank(),
-          legend.background = element_rect(),
-          legend.spacing.x = unit(0,'cm'),
-          legend.spacing.y = unit(0,'cm'),
+      axis.ticks = element_blank(),
 
-          plot.title = element_text(margin=unit(c(0.2,0,0.15,0)*scale,"cm"),size=24*scale, hjust=0.0, face = 'bold'),
-          plot.subtitle = element_text(hjust=0.0,margin=unit(c(0.15,0,0.5,0)*scale,"cm"),size=18*scale),
-          plot.caption = element_text(hjust=0.0,size=14*scale,margin=unit(c(0.25,0,0.15,0)*scale,"cm")),
-          plot.caption.position = "plot",
-          plot.title.position = "plot",
+      axis.text = element_text(
+        angle = 0,
+        colour="#495057",
+        size=18*scale
+        ),
 
-          legend.position = "bottom",
-          legend.direction = "horizontal",
-          legend.box = "vertical",
+      axis.text.x = element_text(
+        margin = margin(t = 0.35 * scale, r = 0.35 * scale, b = 0, l = 0.5 * scale, unit = "cm")
+        ),
 
-          panel.grid = element_line(colour = NULL),
-          panel.grid.major = element_line(colour = "#D2D2D2"),
-          panel.grid.minor = element_blank(),
-          plot.margin = unit(c(1, 1, 1, 1), "lines"),
+      axis.text.y = element_text(
+        margin = margin(t = 0.5 * scale, r = 0.35 * scale, b = 0.5 * scale, l = 0.5 * scale, unit = "cm")),
 
-          strip.background = element_rect()
+      axis.text.y.right = element_text(
+        margin = margin(t = 0.5 * scale, r = 0.5 * scale, b = 0.5 * scale, l = 0.35 * scale, unit = "cm")),
+
+      axis.line = element_blank(),
+
+      legend.key = element_rect(colour=NA, fill=NA),
+
+      legend.margin = margin(0,0,0,0),
+
+      legend.text = element_text(size = 18*scale),
+
+      legend.title = element_blank(),
+
+      legend.background = element_rect(),
+
+      legend.spacing.x = unit(0,'cm'),
+
+      legend.spacing.y = unit(0,'cm'),
+
+      plot.title = element_text(
+        margin = margin(t = 0.2 * scale, r = 0, b = 0.15 * scale, l = 0, unit = "cm"),
+        size=24*scale,
+        hjust=0.0,
+        face = 'bold',
+        colour = "#000000"
+        ),
+
+      plot.subtitle = element_text(
+        hjust = 0.0,
+        margin = margin(t = 0.15 * scale, r = 0, b = 0.5 * scale, l = 0, unit = "cm"),
+        size = 18*scale
+        ),
+
+      plot.caption = element_text(
+        hjust=0.0,
+        size=14*scale,
+        margin = margin(t = 0.25 * scale, r = 0, b = 0.15 * scale, l = 0, unit = "cm")
+        ),
+
+      plot.caption.position = "plot",
+
+      plot.title.position = "plot",
+
+      legend.position = "bottom",
+
+      legend.direction = "horizontal",
+
+      legend.box = "vertical",
+
+      panel.grid = element_line(colour = NULL),
+
+      panel.grid.major = element_line(colour = "#D2D2D2"),
+
+      panel.grid.minor = element_blank(),
+
+      plot.margin = unit(c(1, 1, 1, 1), "lines"),
+
+      strip.background = element_rect()
     )
 }
---
+++ R/plot_label.R
@@ -0,0 +1,124 @@
+#' Add on-graph labels to graphs
+#'
+#' @description Add text labels for lines, columns or other elements directly
+#'   onto the graph plot. This is preferred over using legends.
+#'
+#' @param label String vector. Label text to be displayed.
+#' @param x Numeric or string vector. X-axis positions of the label text.
+#' @param y Numeric or string vector. Y-axis positions of the label text.
+#' @param colour (optional) Vector of colour names or strings. Default uses the
+#'   e61 palette.
+#' @param size (optional) Integer. Size of the text, the default size should be
+#'   appropriate in most cases.
+#' @param hjust (optional) A numeric value from 0-1. Adjusts the alignment of
+#'   the text. 0 left-aligns (default), 0.5 centre-aligns and 1 right-aligns.
+#' @param geom (optional) String. Either "text" (default) or "label". "label"
+#'   adds a white box around the text which could be useful sometimes.
+#' @param angle (optional) Numeric. Rotate the labels. Defaults to 0 which is
+#'   normal left-to-right text.
+#' @param facet_name,facet_value (optional) String. Specify the name of the
+#'   facetting variable in `facet_name` and the panel to show the labels in
+#'   using `facet_value`. Defaults to NULL which shows the labels on all facets.
+#'   You must specify both `facet_name` and `facet_value` or leave both as NULL.
+#'
+#' @return ggplot2 object
+#' @export
+
+plot_label <-
+  function(label,
+           x,
+           y,
+           colour = NA,
+           size = 3.5,
+           hjust = 0,
+           geom = c("text", "label"),
+           angle = 0,
+           facet_name = NULL,
+           facet_value = NULL
+  ) {
+
+    if (!all.equal(length(label), length(x), length(y)))
+      stop("The number of x and y positions must equal the number of labels.")
+
+    geom <- match.arg(geom)
+
+    if (!is.null(facet_name) && length(facet_name) != 1)
+      stop("facet_name must be a string of length 1.")
+
+    if (xor(is.null(facet_value), is.null(facet_name)))
+      stop("You must provide both `facet_name` and `facet_value` or leave both as NULL.")
+
+    esc_pal <- esc_palette()[length(label)]
+    # Set up colours
+    if (length(colour) == 1 && is.na(colour)) {
+      colour <- esc_pal
+    } else if (length(colour) == 1 && !is.na(colour)) {
+      colour <- rep(colour, length(label))
+    } else if (length(colour) != length(label)) {
+      stop("The number of colours must equal the number of labels.")
+    }
+
+    # Set up facets
+    if (!is.null(facet_name) && length(facet_value) == 1) {
+      facet_value <- rep(facet_value, length(label))
+    } else if (!is.null(facet_value) && length(facet_value) != length(label)) {
+      stop("facet_value must be 1 or equal to the number of labels.")
+    }
+
+    # Automatically convert dates to dates if specified, so the user doesn't have
+    # to wrap dates in as.Date() which saves some room.
+    try_date <- try(as.Date(as.character(x)), silent = TRUE)
+    if (!inherits(try_date, "try-error")) {
+      x <- try_date
+    }
+
+    # Arg length checking helper
+    len_chk <- function(vec, len = length(label)) {
+      if (length(vec) == len) return(vec)
+      if (length(vec) != 1) stop(substitute(vec), " must be length ", length(label), " or 1.")
+      return(rep(vec, len))
+    }
+
+    plot_lab_data <- data.table::data.table(
+      label = label,
+      x = x,
+      y = y,
+      colour = colour,
+      size = len_chk(size),
+      hjust = len_chk(hjust),
+      angle = len_chk(angle),
+      facet = facet_value
+    )
+
+    # Facet column needs to have the same name as the facetting variable
+    if (!is.null(facet_name)) {
+      data.table::setnames(plot_lab_data, old = "facet", new = facet_name)
+    }
+
+    # Create the geom
+    if (geom == "text") {
+      retval <- geom_text(
+        data = plot_lab_data,
+        mapping = aes(x, y, label = label),
+        colour = colour, size = size, hjust = hjust, angle = angle,
+        inherit.aes = FALSE
+      )
+    } else if (geom == "label") {
+      retval <- geom_label(
+        data = plot_lab_data,
+        mapping = aes(x, y, label = label),
+        colour = colour, size = size, hjust = hjust, angle = angle,
+        inherit.aes = FALSE
+      )
+    }
+
+    # Only add the functionality to adjust the size of the plot labels if the
+    # user did not manually enter a size
+    if (size == 3.5) {
+      attr(retval, "adj_plot_label") <- TRUE
+    }
+
+    return(retval)
+
+  }
+
